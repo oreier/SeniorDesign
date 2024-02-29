ab3b1121a3c1344de110e7c4847a3474540b491d
Delaney Lim <lim@mines.edu>
2024-02-29 09:55:49 -0700

    lots of updates on final code, need to fix delay, need to fix up compoents

--- SeniorDesignFirst/SeniorDesignFirst.ino
+++ SeniorDesignFirst/SeniorDesignFirst.ino
@@ -1,26 +1,39 @@
+//LIRBARIES!!!
 #include <Servo.h>
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
 
+
+// DEFINITIONS
 #define SOUND_INTERVAL 10000   // Interval between sound plays in milliseconds
-#define TRIGGER_PIN 2          // Pin number for the trigger input
-#define ARM_SERVO_PIN 9        // Pin number for the arm servo motor
+
+#define ELBOW_LEFT_SERVO_PIN 8        // Pin number for the elbow left servo motor
+#define ELBOW_RIGHT_SERVO_PIN 9        // Pin number for the elbow right servo motor
 
 #define HAND_LEFT_SERVO_PIN 10    // Pin number for the hand left servo
 #define HAND_RIGHT_SERVO_PIN 11    // Pin number for the hand right servo
+int minServo = 500;
+int maxServo = 2500;
 
 #define LEFT_LINEAR_ACTUATOR_PIN 5   // Pin number for the left shoulder linear actuator
 #define RIGHT_LINEAR_ACTUATOR_PIN 6  // Pin number for the right shoulder linear actuator
 
 #define LED_PIN_RED 12      // pin number for red pin led
 #define  LED_PIN_GREEN 13 //pin number for green pin led
-#define LED_PIN_BLUE 13 //pin number for green pin led
+#define LED_PIN_BLUE 13 //pin number for blue pin led
+
+#define int red = LOW
+#define int blue = LOW
+#define int yellow = LOW
 
 #define const int purple[] = {128,0,128}
 #define const int green[] = {0,255,0}
 #define const int orange[] = {255,50,0}
 
-#define const int red = LOW
-#define const int blue = LOW
-#define const int yellow = LOW
+#define const int RED_THRESHOLD = 50000;
+#define const int BLUE_THRESHOLD = 30000;
+#define const int YELLOW_THRESHOLD = 45000;
+
 
 enum RobotState {
   IDLE,
@@ -28,41 +41,79 @@ enum RobotState {
 };
 
 
-
+// VARIABLES
 RobotState currentState = IDLE;
 unsigned long previousSoundTime = 0;
 
 Servo handLeftServo;
 Servo handRightServo;
 
-//Servo leftElbow;
-//Servo rightELbow;
+Servo leftElbowServo;
+Servo rightELbowServo;
+ 
+Adafruit_TCS34725 colorSensor1 = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
+Adafruit_TCS34725 colorSensor2 = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
 
 void setup() {
-  pinMode(TRIGGER_PIN, INPUT_PULLUP);
+
   pinMode(LEFT_LINEAR_ACTUATOR_PIN, OUTPUT);
   pinMode(RIGHT_LINEAR_ACTUATOR_PIN, OUTPUT);
+
   pinMode(LED_PIN_RED, OUTPUT);
   pinMode(LED_PIN_GREEN, OUTPUT);
   pinMode(LED_PIN_BLUE, OUTPUT);
   
-  handLeftServo.attach(HAND_LEFT_SERVO_PIN);
-  handRightServo.attach(HAND_RIGHT_SERVO_PIN);
+  leftHandServo.attach(HAND_LEFT_SERVO_PIN, minServo, maxServo);
+  rightHandServo.attach(HAND_RIGHT_SERVO_PIN, minServo, maxServo);
+
+  leftElbowServo.attach(ELBOW_LEFT_SERVO_PIN, minServo, maxServo);
+  rightElbowServo.attach(ELBOW_RIGHT_SERVO_PIN, minServo, maxServo);
+
+  //set pins. Optional for Wire, not for Wire1
+  Wire1.setSDA(2);
+  Wire1.setSCL(3);
+  Wire.setSDA(4);
+  Wire.setSCL(5);
+
+  //initialize sensor 1 and check initialization simultaneously
+  if (colorSensor1.begin(TCS34725_ADDRESS, &Wire)) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1);
+  }
+
+  //initialize sensor 2 and check initialization simultaneously
+  if (colorSensor2.begin(TCS34725_ADDRESS, &Wire1)) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1);
+  }
   
   // Additional setup code for other motors or sensors
 }
 
-void loop() {
-  switch (currentState) {
-    case IDLE:
-      idleState();
-      break;
+// void loop() {
+//   switch (currentState) {
+//     case IDLE:
+//       idleState();
+//       break;
       
-    case MOVEMENT:
-      movementState();
-      break;
+//     case MOVEMENT:
+//       movementState();
+//       break;
+//   }
+// }
+
+void loop() {
+  if (currentState == IDLE) {
+    idleState();
+  } else if (currentState == MOVEMENT) {
+    movementState();
   }
 }
+
 
 void idleState() {
   unsigned long currentTime = millis();
@@ -80,9 +131,8 @@ void idleState() {
   }
 
   // Add slight movement
-  int angle = map(currentTime % 2000, 0, 2000, 0, 180); // Adjust values 
-  myServo.write(angle);
-  delay(15); // Adjust delay based on your servo speed 
+  rightHandServo.write(45); //CHECK?????
+  delay(1000); // Adjust delay based on your servo speed 
 }
 
 void movementState() {
@@ -93,9 +143,10 @@ void movementState() {
   liftArmElbow();
   turnOnLED();
   setColorOff();
-  returnArmShoulder();
   returnArmElbow();
-  releaseServoAndElectromagnet();
+  returnArmShoulder();
+  openHand();
+  releaseElectromagnet();
   
   currentState = IDLE;  // Transition back to idle state
 }
@@ -109,13 +160,51 @@ void engageElectromagnet() {
 }
 
 void closeHand() {
-  fingerServo.write(90);  // Move the finger servo to 90 degrees
-  delay(200);             // Adjust the delay as needed
+  leftHandServo.write(180);  // closed hand is 180     
+  rightHandServo.write(180);  // closed hand is 180
+  delay(1000);                    
 }
 
-colorSensor(){
-  // code to set 2 colors
+void colorSensor(){
+  //redundant copies of variables are included to process both sets of variables independently
+  uint16_t r, g, b, c, colorTemp, lux;
+  
+  uint16_t r1, g1, b1, c1, colorTemp1, lux1;
+
+  colorSensor.getRawData(&r, &g, &b, &c);
+  // colorTemp = tcs.calculateColorTemperature(r, g, b);
+  colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
+  lux = tcs.calculateLux(r, g, b);
+
+  colorSensor1.getRawData(&r1, &g1, &b1, &c1);
+  // colorTemp = tcs.calculateColorTemperature(r, g, b);
+  colorTemp1 = tcs1.calculateColorTemperature_dn40(r1, g1, b1, c1);
+  lux1 = tcs1.calculateLux(r1, g1, b1);
+
+  setBeakerColors(r, g, b);
+  setBeakerColors(r1, g1, b1);
+
 }
+
+void setBeakerColors(uint16_t r, uint16_t g, uint16_t b) {
+  // Red detected
+  if (r > RED_THRESHOLD && g < YELLOW_THRESHOLD && b < YELLOW_THRESHOLD) {
+    red = HIGH;
+  }
+
+  // Blue detected
+  else if (r < BLUE_THRESHOLD && g < BLUE_THRESHOLD && b > BLUE_THRESHOLD) {
+    blue = HIGH;
+  }
+
+  // Yellow detected
+  else if (r > YELLOW_THRESHOLD && g > YELLOW_THRESHOLD && b < BLUE_THRESHOLD) {
+    yellow = HIGH;
+  }
+}
+
+// ************************* This way of doing the arm shoulders has got to change 
+// because both shoulders should move at once 
 
 void liftArmShoulder() {
   liftArmShoulderLeft();
@@ -136,7 +225,17 @@ void liftArmShoulderRight() {
   digitalWrite(RIGHT_LINEAR_ACTUATOR_PIN, LOW);
 }
 
-void turnOnLED() {
+void extendArmElbow() {
+
+// *********************** WHAT IS THIS ANGLE??? ELBOW
+
+  leftElbowServo.write(180);  // left elbow
+  rightElbowServo.write(180); // right elbow
+  delay(1000); 
+}
+
+
+void turnOnLED(uint16_t r, uint16_t g, uint16_t b)) {
   if (red == HIGH && yellow == HIGH) {
     setColor(orange);
   }
@@ -165,14 +264,54 @@ void setColorOff() {
   digitalWrite(LED_PIN_BLUE, LOW); // set blue pin off 
 }
 
+void returnArmElbow() {
 
-void returnArmsToSide() {
-  // Code to return the arms back to the side (0-degree position)
+// *********************** WHAT IS THIS ANGLE??? ELBOW
+
+  leftElbowServo.write(180);  // left elbow
+  rightElbowServo.write(180); // right elbow
+  delay(1000); 
+
 }
 
-void releaseServoAndElectromagnet() {
-  // Code to release the servo and electromagnet
+// ************************* This way of doing the arm shoulders has got to change 
+// because both shoulders should move at once 
+
+void returnArmShoulder() {
+  returnArmShoulderLeft();
+  returnArmShoulderRight();
 }
+
+void returnArmShoulderLeft() {
+  // Code to move the left shoulder linear actuator to return the arm elbow to a 0 degree
+  digitalWrite(LEFT_LINEAR_ACTUATOR_PIN, HIGH);
+  delay(2000); // adjust the delay time as needed to reach position
+  digitalWrite(LEFT_LINEAR_ACTUATOR_PIN, LOW);
+}
+
+void returnArmShoulderRight() {
+  // Code to move the right shoulder linear actuator to return the arm elbow to a 0 degree
+  digitalWrite(RIGHT_LINEAR_ACTUATOR_PIN, HIGH);
+  delay(2000); // adjust the delay time as needed to reach position
+  digitalWrite(RIGHT_LINEAR_ACTUATOR_PIN, LOW);
+}
+
+}
+
+
+void openHand() {
+
+  leftHandServo.write(0);  // open hand is     
+  rightHandServo.write(0);  // open hand is ?????WHAT ANGLE
+  delay(1000);  
+
+}
+
+void releaseElectromagnet() {
+  // Code to release the electromagnet
+}
+
+
 
 void playSound() {
   // Code to play the sound
