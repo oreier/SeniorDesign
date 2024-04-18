4e6398bacfd1fdf3bf62650815a669c8139ea420
Delaney Lim <lim@mines.edu>
2024-04-18 13:00:43 -0600

    middle LED working

--- SeniorDesign2ndredition/SeniorDesign2ndredition.ino
+++ SeniorDesign2ndredition/SeniorDesign2ndredition.ino
@@ -32,10 +32,6 @@
 #define ELECTRO_MAG_RIGHT_PIN 14    // pin number for right elctro magnet 
 #define ELECTRO_MAG_LEFT_PIN 15    // pin number for left elctro magnet 
 
-#define LED_PIN_BLUE 16 //pin number for blue pin led
-#define  LED_PIN_GREEN 17 //pin number for green pin led
-#define LED_PIN_RED 18     // pin number for red pin led
-
 #define LED_PIN     16
 #define LED_COUNT  44 // how many led's are the the led strip
 // NeoPixel brightness, 0 (min) to 255 (max)
@@ -53,7 +49,7 @@ int yellow = 0;
 
 const int purple[] = {128,0,128};
 const int green[] = {0,255,0};
-const int orange[] = {255,50,0};
+const int orange[] = {255, 69, 0};
 
 const int RED_THRESHOLD = 1000;
 const int BLUE_THRESHOLD = 30000;
@@ -69,6 +65,19 @@ unsigned long proximityLeft = 0;
 
 char soundYawn[] = "/00001*MP3";
 char soundBloop[] = "/00002*MP3";
+char soundBloop2[] = "/00003*MP3";
+char greenBeaker[] = "/00004*MP3";
+char purpleBeaker[] = "/00005*MP3";
+char orangeBeaker[] = "/00006*MP3";
+
+
+// // Array of sound file names
+// char* soundPaths[] = {
+//   "/00002*MP3",
+//   "/00003*MP3"
+// };
+
+int numberOfSounds = 2;
 
 enum Sides{
   LEFT,
@@ -98,13 +107,13 @@ Servo rightElbowServo;
 Adafruit_APDS9960 colorSensorR;
 Adafruit_APDS9960 colorSensorL;
 
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 bool colorDetected = false;
 
-int LHandClosed = 20;
+int LHandClosed = 35;
 int LHandOpen = 70;
-int RHandClosed = 95;
+int RHandClosed = 80;
 int RHandOpen = 30;
 
 int RBowBent = 10;
@@ -112,18 +121,12 @@ int RBowStraight = 85;
 int LBowBent = 90;
 int LBowStraight = 15;
 
-
-
 void setup() {
   
   pinMode(RIGHT_LINEAR_ACTUATOR_PIN_UP, OUTPUT);
   pinMode(LEFT_LINEAR_ACTUATOR_PIN_UP, OUTPUT);
   pinMode(RIGHT_LINEAR_ACTUATOR_PIN_DOWN, OUTPUT);
   pinMode(LEFT_LINEAR_ACTUATOR_PIN_DOWN, OUTPUT);
-
-  pinMode(LED_PIN_RED, OUTPUT);
-  pinMode(LED_PIN_GREEN, OUTPUT);
-  pinMode(LED_PIN_BLUE, OUTPUT);
   
   handLeftServo.attach(HAND_LEFT_SERVO_PIN, minServo, maxServo);
   handRightServo.attach(HAND_RIGHT_SERVO_PIN, minServo, maxServo);
@@ -211,18 +214,19 @@ void loop() {
 void idleState() {
   unsigned long currentTime = millis();
   
-  /*
-  // Check if the trigger pin is triggered
-  if (digitalRead(TRIGGER_PIN) == LOW) {
-    currentState = MOVEMENT;  // Transition to movement state
-    initializeMovementState();
-  }
-
-  */
   proximityRight = colorSensorR.readProximity(); 
   // check if proximity trigger than there must be a beaker in the hand
   if (proximityRight > PROXIMITY_THRESHOLD) {
     Serial.println("beaker in hand, moving to movement state");
+    digitalWrite(ELECTRO_MAG_RIGHT_PIN, HIGH);
+    handRightServo.write(RHandClosed);  // closed hand is 180
+    currentState = MOVEMENT; // transition to movement state
+  }
+
+  else if (proximityLeft > PROXIMITY_THRESHOLD) {
+    Serial.println("beaker in hand, moving to movement state");
+    digitalWrite(ELECTRO_MAG_LEFT_PIN, HIGH);
+    handLeftServo.write(LHandClosed);
     currentState = MOVEMENT; // transition to movement state
   }
   
@@ -230,7 +234,8 @@ void idleState() {
   if (currentTime - previousMoveTime >= 5000) {
     previousMoveTime = currentTime;
     Serial.println("in idle state move!"); 
-    playSound();
+    player.playSpecifiedDevicePath(DY::Device::Sd, soundBloop);
+    //playRandomSound();
     slightMovement(); // Call function to move the servo
   }
 
@@ -243,12 +248,21 @@ void slightMovement(){
   handLeftServo.write(LHandOpen);
   delay(3000); // Adjust delay based on your servo speed 
 }
-void playSound() {
-  // Code to play the sound
-  player.playSpecifiedDevicePath(DY::Device::Sd, soundBloop);
-  //player.playSpecified(1);
-}
 
+// void playRandomSound() {
+//   // Generate a random index to select a sound
+//   int randomIndex = random(0, numberOfSounds);
+
+//   // Get the selected sound path
+//   char selectedSound = soundPaths[randomIndex];
+
+//   // Print the selected sound for debugging
+//   Serial.print("Playing sound: ");
+//   Serial.println(selectedSound);
+
+//   // Play the selected sound
+//   player.playSpecifiedDevicePath(DY::Device::Sd, selectedSound);
+// }
 // MOVEMENT STATE****************************************************************************************************
 
 
@@ -277,7 +291,6 @@ void engageElectromagnet() {
 
   // Code to engage the electromagnet
   Serial.println("electro magnets engaged");
-  digitalWrite(ELECTRO_MAG_RIGHT_PIN, HIGH);
   digitalWrite(ELECTRO_MAG_LEFT_PIN, HIGH);
   delay(1000);
 
@@ -318,14 +331,7 @@ void colorSensing(){
   }
 
   colorSensorR.getColorData(&r, &g, &b, &c);
-  // colorTemp = colorSensorR.calculateColorTemperature(r, g, b);
-  //colorTemp = colorSensorR.calculateColorTemperature_dn40(r, g, b, c);
-  //lux = colorSensorR.calculateLux(r, g, b);
-
   colorSensorL.getColorData(&r1, &g1, &b1, &c1);
-  // colorTemp = colorSensorL.calculateColorTemperature(r, g, b);
-  //colorTemp1 = colorSensorL.calculateColorTemperature_dn40(r1, g1, b1, c1);
-  //lux1 = colorSensorL.calculateLux(r1, g1, b1);
 
   setBeakerColors(r, g, b, c);
   setBeakerColors(r1, g1, b1, c1);
@@ -375,37 +381,54 @@ void liftArmShoulder() {
 }
 
 void extendArmElbow() {
+  // leftElbowServo.write(LBowBent);  // left elbow
+  // rightElbowServo.write(RBowBent); // right elbow
+  // Serial.println("done extending elbow!!!!!");
+  // delay(1000); 
 
-// *********************** angle 90 - 180
+  int increment = 1;  // Increment value for gradual movement
+  int delayTime = 20; // Delay time in milliseconds between each increment
 
-  //for(pos = 0; pos<=90; pos+=1);
+  int currentPosLeft = 90;   // Read current position of left elbow servo
+  int currentPosRight = rightElbowServo.read(); // Read current position of right elbow servo
 
-  //Serial.println("extending elbow!!!!!");
-  leftElbowServo.write(LBowBent);  // left elbow
-  rightElbowServo.write(RBowBent); // right elbow
-  Serial.println("done extending elbow!!!!!");
-  delay(1000); 
+  // Extend left elbow servo gradually to LBowBent position
+  if (currentPosLeft < LBowBent) {
+    for (int pos = currentPosLeft; pos <= LBowBent; pos -= increment) {
+      leftElbowServo.write(pos);
+      delay(delayTime);
+    }
+  }
+  if (currentPosRight < RBowBent) {
+    for (int pos = currentPosRight; pos <= RBowBent; pos += increment) {
+      rightElbowServo.write(pos);
+      delay(delayTime);
+    }
+  }
 }
 
 
 void turnOnLED() {
   Serial.println("LED!!!!!");
   if (red == 1 && yellow == 1) {
+    player.playSpecifiedDevicePath(DY::Device::Sd, orangeBeaker);
     setColor(orange);
   }
   else if(red == 1 && blue == 1) {
     Serial.println("Purple!!!");
+    player.playSpecifiedDevicePath(DY::Device::Sd, purpleBeaker);
     setColor(purple);
   }
   else if(yellow == 1 && blue == 1) {
+    player.playSpecifiedDevicePath(DY::Device::Sd, greenBeaker);
     setColor(green);
   }
   else { // invalid combination
-    //setColorOff();
-    setColor(orange);
+    setColorOff();
+    //setColor(orange);
   }
 
-  delay(10000);
+  delay(8000);
 }
 
 void setColor(const int color[]) {
@@ -413,25 +436,16 @@ void setColor(const int color[]) {
   uint8_t r = color[0];  // Extract red value from array
   uint8_t g = color[1];  // Extract green value from array
   uint8_t b = color[2];  // Extract blue value from array
-  
-  //for(int i=0; i<LED_COUNT; i++) { // For each pixel...
 
-    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-    //strip.setPixelColor(i, strip.Color(r, g, b));
-
-    //strip.show();   // Send the updated pixel colors to the hardware.
-  //}
-
-  strip.fill(strip.Color(r, g, b), 0, LED_COUNT);
-  strip.show();
+  colorWipe(strip.Color(r, g, b), 50);
   
 }
 
 void setColorOff() {
-  strip.fill(strip.Color(0, 0, 0), 0, LED_COUNT);
-  strip.show();
-  //rainbow(10);
-  
+  colorWipe(strip.Color(0, 0, 0), 50);
+  red = 0;
+  yellow = 0;
+  blue = 0;
 }
 
 void returnArmElbow() {
@@ -467,22 +481,10 @@ void releaseElectromagnet() {
   digitalWrite(ELECTRO_MAG_LEFT_PIN, LOW);
 }
 
-// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-void rainbow(int wait) {
-  // Hue of first pixel runs 5 complete loops through the color wheel.
-  // Color wheel has a range of 65536 but it's OK if we roll over, so
-  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
-  // means we'll make 5*65536/256 = 1280 passes through this loop:
-  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-    // strip.rainbow() can take a single argument (first pixel hue) or
-    // optionally a few extras: number of rainbow repetitions (default 1),
-    // saturation and value (brightness) (both 0-255, similar to the
-    // ColorHSV() function, default 255), and a true/false flag for whether
-    // to apply gamma correction to provide 'truer' colors (default true).
-    strip.rainbow(firstPixelHue);
-    // Above line is equivalent to:
-    // strip.rainbow(firstPixelHue, 1, 255, 255, true);
-    strip.show(); // Update strip with new contents
-    delay(wait);  // Pause for a moment
+void colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<LED_COUNT; i++) {
+    strip.setPixelColor(i, color);
+    strip.show();
+    delay(wait);
   }
 }
